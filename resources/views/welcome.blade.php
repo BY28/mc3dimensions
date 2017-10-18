ad1d920dd68a4d4607d91159badb18ada5ee7b31
BY28 <b_yanis_@hotmail.fr>
2017-10-18 12:09:19 +0000

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MC3Dimensions Laravel
                 </div>
 
                 <div class="links">
